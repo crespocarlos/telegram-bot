6890653d782d202d8a60f3be233940c860f02031
ccrespo-ciandt <carlos.hcrespo@gmail.com>
2017-05-27 16:45:01 -0300

    adding gitignore

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Used for production build
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, function() {
+    console.log('Server running on ' + PORT);
+});
